587a1d580d19a8a4dcf2be303374fa9e87093ad0
Eric Wang <ericwang27759@gmail.com>
2019-09-18 15:16:52 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/*addItemHandler() handles when a user adds a new item
+ to the shopping list.*/
 function addItemHandler() {
     $("#js-shopping-list-form").submit(function(event) {
         event.preventDefault();
-        const item = $(this).find("#shopping-list-entry").val();
+        const item = $("#shopping-list-entry").val();
         $(".shopping-list").append(`<li>
             <span class="shopping-item">${item}</span>
             <div class="shopping-item-controls">
@@ -14,24 +16,32 @@ function addItemHandler() {
             </div>
           </li>`);
 
-          //with item.val(""), I get an error saying 
-          //item.val() is not a function?
-          $(this).find("#shopping-list-entry").val("");
+          $("#shopping-list-entry").val("");
     });
 }
 
+/*deleteItemHandler() handles when a user clicks the "delete" button
+to delete an item from the shopping list.*/
 function deleteItemHandler() {
     $("ul").on("click", ".shopping-item-delete", function(event) {
         $(this).closest("li").remove();
     });
 }
 
+/*checkUncheckItemHandler() handles when a user clicks the "check" button
+to check/uncheck an item from the shopping list.*/
 function checkUncheckItemHandler() {
     $("ul").on("click", ".shopping-item-toggle", function(event) {
-        $(this).closest(".shopping-item").toggleClass("shopping-item__checked");
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
     });
 }
 
-$(addItemHandler);
-$(deleteItemHandler);
-$(checkUncheckItemHandler);
+/*shoppingListHandler() is the callback function when the page loads,
+and activates all the other functions.*/
+function shoppingListHandler() {
+  addItemHandler();
+  deleteItemHandler();
+  checkUncheckItemHandler();
+}
+
+$(shoppingListHandler);
